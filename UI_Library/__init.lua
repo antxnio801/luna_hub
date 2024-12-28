883b95d69722baf2943985b3bbf7f2a211b796ef
antxnio801 <antxniopro1234@gmail.com>
2024-12-28 12:51:32 -0700

    Update __init.lua

--- UI_Library/__init.lua
+++ UI_Library/__init.lua
@@ -4,13 +4,7 @@ local lib = {
 }
 
 function lib:__int()
-		
-	print("A")
-	
-	task.wait(math.random(1,  3))
-	
-	print("XD")
-	
+
 	lib.__i = true
 	lib.__initialized:Fire()
 end
@@ -20,8 +14,6 @@ function lib:CheckInitialized()
 	if not lib.__i then
 		lib.__initialized.Event:Wait()
 	end
-	
-	print("SI")
 	return lib
 end
 
